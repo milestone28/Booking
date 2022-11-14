36ac2519e2002b76ebf0350319a1375ed0785e41
Gary Yu <garyjohn.yu@got-the-experts.com>
2022-11-14 13:36:02 +0800

    Add project files.

---
+++ Booking.Api/Controllers/HotelController.cs
@@ -0,0 +1,80 @@
+﻿using Booking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Booking.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HotelController : Controller
+    {
+        private readonly DataSource _dataSource;
+
+        public HotelController(DataSource dataSource)
+        {
+            _dataSource = dataSource;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllHotels()
+        {
+            var hotels = _dataSource.Hotels;
+            return Ok(hotels);
+        }
+
+        [Route("{id}")]
+        [HttpGet]
+
+        public IActionResult GetHotelById(int id)
+        {
+            var hotels = _dataSource.Hotels;
+            var hotel = hotels.FirstOrDefault(h => h.HotelId == id);
+            if(hotel == null)
+            {
+                return NotFound();
+            }
+            return Ok(hotel);
+        }
+
+        [HttpPost]
+        public IActionResult CreateHotel([FromBody] Hotel hotel)
+        {
+            var hotels = _dataSource.Hotels;
+            hotels.Add(hotel);
+
+            return CreatedAtAction(nameof(GetHotelById),new { id = hotel.HotelId}, hotel);
+        }
+
+        [Route("{id}")]
+        [HttpPut]
+        public IActionResult UpdateHotel([FromBody] Hotel updated, int id)
+        {
+            var hotels = _dataSource.Hotels;
+            var oldHotel = hotels.FirstOrDefault(h => h.HotelId == id);
+
+            if (oldHotel == null)
+                return NotFound("No resource with the corresponding ID found");
+
+            hotels.Remove(oldHotel);
+            hotels.Add(updated);
+            return NoContent();
+        }
+
+        [Route("{id}")]
+        [HttpDelete]
+        public IActionResult DeleteHotel(int id)
+        {
+            var hotels = _dataSource.Hotels;
+            var hotel = hotels.FirstOrDefault(x => x.HotelId == id);
+
+            if (hotel == null)
+                return NotFound("No resource with the corresponding ID found");
+
+            hotels.Remove(hotel);
+            return NoContent();
+        }
+
+        
+    }
+}
---
+++ Booking.Api/DataSource.cs
@@ -0,0 +1,42 @@
+﻿using Booking.Domain.Models;
+using System.Collections.Generic;
+
+namespace Booking.Api
+{
+    public class DataSource
+    {
+        public DataSource()
+        {
+            Hotels = GetHotels();
+        }
+        public List<Hotel> Hotels { get; set; }
+
+        private List<Hotel> GetHotels()
+        {
+            return new List<Hotel>
+            {
+                new Hotel
+                {
+                    HotelId = 1,
+                    Name = "Marco Polo",
+                    Stars = 5,
+                    Country = "Phillipines",
+                    Address = "Claveria",
+                    City = "Davao",
+                    Description = "this is description"
+                },
+
+                new Hotel {
+                    HotelId = 2,
+                    Name = "Ayala Land",
+                    Stars = 4,
+                    Country = "Phillipines",
+                    Address = "Claveria",
+                    City = "Davao",
+                    Description = "this is description"
+
+                }
+            };
+        }
+    }
+}
